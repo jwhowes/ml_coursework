c1ace4ed25f7ace776fd5c3fcb07c98fdc54f7a6
jwhowes <jacob.w.howes@durham.ac.uk>
2020-04-20 13:25:12 +0100

    Added comments

--- classifier.py
+++ classifier.py
@@ -18,16 +18,16 @@ import collections
 #### How to run:																	 ####
 ####	- Ensure matplotlib, sklearn, numpy, and pandas are installed and up-to-date ####
 ####	- Run via the commmand line or through other means							 ####
-####		- e.g. python classifier.py												 ####
+####		- e.g. $ python classifier.py											 ####
 ####	- All data files should be stored as .csv in a subfolder called 'data'		 ####
 #########################################################################################
 
 show_corr = False
-show_hist = False
-show_strat_hist = False
+show_hist = True
 do_grid_search = False
 
 def result_to_int(result):
+	"""Converts final_result attribute to an integer"""
 	if result == "Distinction":
 		return 2
 	elif result == "Pass":
@@ -35,15 +35,17 @@ def result_to_int(result):
 	elif result == "Fail":
 		return 0
 
-def int_to_pass_fail(num):  # If we're including withdrawn rows
+def int_to_pass_fail(num):
+	"""Converts result to pass/fail (pass = 1, fail = 0)"""
 	if num == 2 or num == 1:
 		return 1
 	return 0
 
 def show_metrics(X, y, classifier, multiclass=False):
+	"""Displays metrics for a classifier acting on data X with true labels y"""
 	pred_y = classifier.predict(X)
 	if multiclass:
-		print("Precision:", precision_score(y, pred_y, average="macro"))
+		print("Precision:", precision_score(y, pred_y, average="macro"))  # Uses macro averaging for multiclass classifiers
 		print("Recall:", recall_score(y, pred_y, average="macro"))
 	else:
 		print("Precision:", precision_score(y, pred_y))
@@ -55,41 +57,44 @@ def show_metrics(X, y, classifier, multiclass=False):
 		print("F1:", f1_score(y, pred_y))
 
 def show_corr_matrix(data, label):
+	"""Displays the correlation matrix for data with a given label"""
 	corr_matrix = data.corr()
 	print(corr_matrix[label].sort_values(ascending=False))
 
 def plot_scatter_matrices(data, attributes):
+	"""Plots the scatter matrix for dataset 'data' and attribute names 'attributes'"""
 	matrix = pd.plotting.scatter_matrix(students[attributes], figsize=(15,15), )
 	for ax in matrix.ravel():
 		ax.set_xlabel(ax.get_xlabel(), fontsize=15, rotation=0)
 		ax.set_ylabel(ax.get_ylabel(), fontsize=15, rotation=90)
-	plt.savefig("scatter_matrix.png")
+	plt.show()
 
 def plot_logistic_classifier(X_unprepared, X_prepared, x, labels, log_classifier):
+	"""Displays a scatter chart showing the distribution of a binary label and the predicted probability of each example against a certain attribute 'x'"""
 	plt.figure(figsize=(4,4))
 	plt.xlabel(x, fontsize=14)
 	plt.ylabel("final_result", fontsize=14)
 	plt.scatter(X_unprepared[x], labels)
 	plt.scatter(X_unprepared[x], log_classifier.predict_proba(X_prepared)[:,1], s=0.1)
-	plt.show()
+	plt.savefig("pass_distinction_log.png")
 
-students = pd.read_csv("./data/studentInfo.csv")  # Data from studentInfo
+# import data
+students = pd.read_csv("./data/studentInfo.csv")
 registration = pd.read_csv("./data/studentRegistration.csv")
 assessment = pd.read_csv("./data/studentAssessment.csv")
 
-#students = students.dropna(subset=["imd_band"])  # Drop students with null imd_band attribute (not used anymore)
-
-students = students[students["final_result"] != "Withdrawn"]  # The correlations are much higher when we remove withdrawn values
+students = students[students["final_result"] != "Withdrawn"]  # Removes withdrawn students
 
 #################################
 #### Creating Aggregate data ####
 #################################
 
-assessment_agg = assessment.groupby('id_student').agg(
+assessment_agg = assessment.groupby('id_student').agg(  # Creates avg_score aggregate field
 	avg_score=pd.NamedAgg(column='score', aggfunc=np.mean)
 )
-students = pd.merge(students, assessment_agg, on='id_student')  # Adding average score for each student
+students = pd.merge(students, assessment_agg, on='id_student')  # Merges students data to add avg_score field to dataset
 
+# Create and merge num_modules aggregate field
 registration_agg = registration.drop(['code_presentation', 'date_registration', 'date_unregistration'], axis=1)
 registration_agg = registration_agg.groupby('id_student').count()
 registration_agg = registration_agg.rename(columns={
@@ -98,53 +103,44 @@ registration_agg = registration_agg.rename(columns={
 })
 students = pd.merge(students, registration_agg, on='id_student')  # Adding the number of modules a student is taking
 
-# Histograms (pg 55):
-hist = False
+# Displays histograms for numerical student data
 if show_hist:
 	students.hist(bins=50, figsize=(20, 15))
 	plt.show()
 
-# What to do stratified sampling on:
-# Correlation matrix (pg 64):
-students["final_result"] = students["final_result"].map(result_to_int)
+students["final_result"] = students["final_result"].map(result_to_int)  # Convert final_result field to integer
 
+# Displays correlation data for students
 if show_corr:
 	show_corr_matrix(students, "final_result")
 	plot_scatter_matrices(students, ["num_of_prev_attempts", "studied_credits", "num_modules", "avg_score", "final_result"])
-
-students["avg_score_cat"] = np.ceil(students["avg_score"]/20)
-students["avg_score_cat"].where(students["avg_score_cat"] < 5, 5.0, inplace=True)
-
-if show_strat_hist:
-	students["avg_score_cat"].hist()
-	plt.show()
 
 ############################
 #### Stratifed Sampling ####
 ############################
 pass_fail = students.copy()
-pass_fail["final_result"] = pass_fail["final_result"].map(int_to_pass_fail)
+pass_fail["final_result"] = pass_fail["final_result"].map(int_to_pass_fail)  # Create pass_fail dataset: 1=pass, 0=fail
 
 pass_distinction = students.loc[students["final_result"].isin([2, 1])].copy()
-pass_distinction["final_result"] = pass_distinction["final_result"].map(lambda x: x - 1)  # 1: distinction, 0: pass
+pass_distinction["final_result"] = pass_distinction["final_result"].map(lambda x: x - 1)  # Create pass_distinction dataset: 1=distinction, 0=pass
 
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)  # Remove random_state before submitting
-for train_index, test_index in split.split(students, students["final_result"]):
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)  # Begin stratified sampling
+for train_index, test_index in split.split(students, students["final_result"]):  # Create student test and training sets
 	students_train_set = students.iloc[train_index]
 	students_test_set = students.iloc[test_index]
 
-for train_index, test_index in split.split(pass_fail, pass_fail["final_result"]):
+for train_index, test_index in split.split(pass_fail, pass_fail["final_result"]):  # Create pass/fail test and training sets
 	pass_fail_train_set = pass_fail.iloc[train_index]
 	pass_fail_test_set = pass_fail.iloc[test_index]
 
-for train_index, test_index in split.split(pass_distinction, pass_distinction["final_result"]):
+for train_index, test_index in split.split(pass_distinction, pass_distinction["final_result"]):  # Create pass/distinction test and training sets
 	pass_distinction_train_set = pass_distinction.iloc[train_index]
 	pass_distinction_test_set = pass_distinction.iloc[test_index]
 
 for set_ in (students_train_set, students_test_set, pass_fail_train_set, pass_fail_test_set, pass_distinction_train_set, pass_distinction_test_set):
-	set_.drop("avg_score_cat", axis=1, inplace=True)
-	set_.drop("id_student", axis=1, inplace=True)
+	set_.drop("id_student", axis=1, inplace=True)  # Drop id_student (no longer needed and creates unnecessary noise).
 
+# Separate labels from training sets
 students = students_train_set.drop("final_result", axis=1)
 students_labels = students_train_set["final_result"].copy()
 
@@ -158,25 +154,30 @@ pass_distinction_labels = pass_distinction_train_set["final_result"].copy()
 #### Data Cleaning ####
 #######################
 
+# Separating numerical and categorical attributes
 num_attribs = ["num_of_prev_attempts", "studied_credits", "num_modules", "avg_score"]
 cat_attribs = ["code_module", "code_presentation", "gender", "region", "highest_education", "imd_band", "age_band", "disability"]
 
+# Numerical pipeline
 num_pipeline = sklearn.pipeline.Pipeline([
 	('imputer', sklearn.impute.SimpleImputer(strategy="median")),  # Replace null values with the median
 	('std_scaler', sklearn.preprocessing.StandardScaler())  # Standardises data ranges
 ])
 
+# Categorical pipeline
 cat_pipeline = sklearn.pipeline.Pipeline([
-	('imputer', sklearn.impute.SimpleImputer(strategy="most_frequent")),
-	('one_hot', sklearn.preprocessing.OneHotEncoder())
+	('imputer', sklearn.impute.SimpleImputer(strategy="most_frequent")),  # Replace null values with most common for that attribute
+	('one_hot', sklearn.preprocessing.OneHotEncoder())  # One-hot encoding
 ])
 
+# Combine numerical and categorical pipelines
 full_pipeline = sklearn.compose.ColumnTransformer([
 	("num", num_pipeline, num_attribs),
 	("cat", cat_pipeline, cat_attribs)
 ])
 
-students_prepared = full_pipeline.fit_transform(students)
+# Create prepared training sets
+students_prepared = full_pipeline.fit_transform(students)  # Imputers etc. trained against full training set (fail, pass, and distinctions included)
 pass_fail_prepared = full_pipeline.transform(pass_fail)
 pass_distinction_prepared = full_pipeline.transform(pass_distinction)
 
@@ -184,11 +185,13 @@ pass_distinction_prepared = full_pipeline.transform(pass_distinction)
 #### Training the model(s) ####
 ###############################
 
+# Create untrained models
 students_forest = sklearn.ensemble.RandomForestClassifier(random_state=42, oob_score=True)
 pass_fail_log = sklearn.linear_model.LogisticRegressionCV()  # sklearn's CV classifiers contain builit-in cross-validation for hyper paramter tuning.
 pass_distinction_log = sklearn.linear_model.LogisticRegressionCV()
 
-if do_grid_search:  # Very slow for random forests
+# Perform grid search for hyperparameter tuning (quite slow)
+if do_grid_search:
 	param_grid = [{'n_estimators': [50, 75, 100], 'max_features': ["auto", 6, 8]}]
 	grid_search = sklearn.model_selection.GridSearchCV(students_forest, param_grid, cv=5, scoring='roc_auc_ovr', return_train_score=True)
 	grid_search.fit(students_prepared, students_labels)
@@ -204,10 +207,12 @@ if do_grid_search:  # Very slow for random forests
 	print("Pass distinction log:")
 	print(grid_search.best_params_)
 
+# Train models against training data
 students_forest.fit(students_prepared, students_labels)
 pass_fail_log.fit(pass_fail_prepared, pass_fail_labels)
 pass_distinction_log.fit(pass_distinction_prepared, pass_distinction_labels)
 
+# Combine pass/fail and pass/distinction logistic regressors into one regressor
 class Combine_logs(object):
 	def __init__(self, log1, log2):
 		self.log1 = log1
@@ -218,7 +223,6 @@ class Combine_logs(object):
 			if y[i] == 1:
 				y[i] = self.log2.predict(X[i])[0] + 1
 		return y
-
 students_log = Combine_logs(pass_fail_log, pass_distinction_log)
 
 #################################
@@ -241,19 +245,7 @@ pass_distinction_test_prepared = full_pipeline.transform(pass_distinction_test_s
 #### Evaluating the models ####
 ###############################
 
-print("here")
-#students_log = sklearn.linear_model.LogisticRegressionCV(multi_class="multinomial")
-#students_log.fit(students_prepared, students_labels)
-
-#show_metrics(students_test_prepared, students_test_set_labels, students_log, multiclass=True)
-#print("!!!!!!!!!!!!!!!!!!!!!")
-#show_metrics(students_test_prepared, students_test_set_labels, students_forest, multiclass=True)
-show_metrics(students_prepared, students_labels, students_log, multiclass=True)
-print("!!!!!!!!!!!!!!!!!!!")
-show_metrics(students_prepared, students_labels, students_forest, multiclass=True)
-print("Out of bag:", students_forest.oob_score_)
-
-sklearn.metrics.plot_roc_curve(pass_fail_log, pass_fail_prepared, pass_fail_labels)
-plt.show()
-sklearn.metrics.plot_roc_curve(pass_distinction_log, pass_distinction_prepared, pass_distinction_labels)
-plt.show()
+sklearn.metrics.plot_roc_curve(pass_fail_log, pass_fail_test_prepared, pass_fail_test_set_labels)
+plt.savefig("pass_fail_log_test_roc.png")
+sklearn.metrics.plot_roc_curve(pass_distinction_log, pass_distinction_test_prepared, pass_distinction_test_set_labels)
+plt.savefig("pass_distinction_log_test_roc.png")
